5acdcfc228ab10c2641bbbaefb1d8d030a923203
Ted Dorfeuille <teddorfeuille@gmail.com>
2022-07-03 20:43:12 -0400

    test miryoku

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,7 +2,3 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_ALPHAS_QWERTY
-#define MIRYOKU_NAV_INVERTEDT
-#define MIRYOKU_CLIPBOARD_WIN
-#define MIRYOKU_LAYERS_FLIP
-#define MIRYOKU_MAPPING_EXTENDED_THUMBS
