605185252d4e71ece56b5bbf162bcbecac192d99
Ted <Tediferous@users.noreply.github.com>
2024-11-14 15:37:16 -0600

    feat(chocofi): support new keyboard (#1)
    * purge
    
    * pretty layer 1
    
    * pretty
    
    * corne is chocofi?
    
    * view i think
    
    * gem
    
    * test main
    
    * no view
    
    ---------
    
    Co-authored-by: Ted Dorfeuille <ted.dorfeuille@reddit.com>

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# zmk-config
+# Keyboard ZMK Config
+Designed for 36 key boards, based on [miryoku](https://github.com/manna-harbour/miryoku)
 
 
--- miryoku/custom_config.h
+++
@@ -1,5 +0,0 @@
-// Copyright 2021 Manna Harbour
-// https://github.com/manna-harbour/miryoku
-
-#define MIRYOKU_ALPHAS_QWERTY
-#define MIRYOKU_KLUDGE_MOUSEKEYSPR
